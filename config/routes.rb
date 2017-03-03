4bc051c81b8dd773eea9854574207a9ebd014a26
Madalena Pessanha <madalena.lpessanha@gmail.com>
2017-03-03 17:07:15 -0300

    routes for the pages ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+
+  resources :baskets, only: [:new, :create]
+  resources :orders, only: [:new, :create, :update]
+  get "baskets/:id/confirmation", to: "baskets#confirmation"
+  get "orders/:id/success", to: "orders#success"
+  get "orders/:id/failure", to: "orders#failure"
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #get basket/new
 end
