2347c97622e3dbdfa2d20d07087e00a7e59b6950
Madalena Pessanha <madalena.lpessanha@gmail.com>
2017-03-03 17:14:17 -0300

    routes for the pages ok

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,4 @@ Rails.application.routes.draw do
   get "orders/:id/failure", to: "orders#failure"
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  #get basket/new
 end
