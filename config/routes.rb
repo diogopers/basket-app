da694ad4c5ed52a5d719e6833dce2b9ae6889ba4
Alexia Rey <alexiajrey@gmail.com>
2017-03-06 15:14:34 -0300

    show and update profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get "orders/:id/success", to: "orders#success"
   get "orders/:id/failure", to: "orders#failure"
 
+  resources :users, only: [:show, :edit, :update]
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
