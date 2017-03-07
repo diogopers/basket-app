8a4f5faaf62d34b3a17abec9fac48d6e79769eb6
Alexia Rey <alexiajrey@gmail.com>
2017-03-07 14:44:57 -0300

    modifications in address

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   geocoded_by :address
-  after_validation :geocode
+  after_validation :geocode, if: :address_changed?
 
   devise :omniauthable, omniauth_providers: [:facebook]
 
