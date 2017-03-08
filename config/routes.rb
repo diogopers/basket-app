757d0d048fcfbd69fb47ccc5c2b2e6e504228572
Diogo Perin <diogopers@gmail.com>
2017-03-08 16:44:32 -0300

    added counter to shop cart

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   resources :baskets, only: [:new, :create]
   resources :orders, only: [:new, :create, :update]
-  resources :extra_orders, only: [:create, :update]
+  resources :extra_orders, only: [:create, :update, :destroy]
+
   get "baskets/:id/confirmation", to: "baskets#confirmation"
   get "orders/:id/success", to: "orders#success"
   get "orders/:id/failure", to: "orders#failure"
