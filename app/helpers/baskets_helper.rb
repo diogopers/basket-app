2ba032b4c4e258ca77fe01e1a7d96473dc1fa2f0
Diogo Perin <diogopers@gmail.com>
2017-03-09 11:33:42 -0300

    also fixed ajax for cart

--- app/helpers/baskets_helper.rb
+++ app/helpers/baskets_helper.rb
@@ -1,2 +1,11 @@
 module BasketsHelper
+  def initial_quantity(extra)
+    extra_order = extra.extra_orders.find_by(order_id: session[:order_id])
+
+    if extra_order
+      extra_order.quantity
+    else
+      0
+    end
+  end
 end
