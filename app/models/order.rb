605fe3eb42d2b92902924fbb6c66eaf3e21956fa
Madalena Pessanha <madalena.lpessanha@gmail.com>
2017-03-13 13:44:44 -0300

    removed message button from navbar

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :basket
-  belongs_to :delivery_point
+  belongs_to :delivery_point, optional: true
 
   has_many :extra_orders
 
