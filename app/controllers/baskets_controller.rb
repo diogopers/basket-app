283bbb38b4888e7693f97670fbc7325466a344e2
Diogo Perin <diogopers@gmail.com>
2017-03-13 14:13:35 -0300

    fixed map

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,6 +1,4 @@
 class BasketsController < ApplicationController
-
-  before_action :set_delivery_points, only: :pick_address
   skip_before_action :authenticate_user!, only: :new
 
   def new
@@ -20,42 +18,10 @@ class BasketsController < ApplicationController
     @extra_orders = ExtraOrder.where(order_id: session[:order_id])
   end
 
-  def pick_address
-    if session[:order_id].present?
-    @order = Order.find(session[:order_id])
-    end
-
-    respond_to do |format|
-      format.js {
-                  render :template => "baskets/pick_address.js.erb",
-                  :layout => false
-          }
-    end
-
-  end
 
   private
 
-  def set_delivery_points
 
-    if @order.present? && @order.id != nil
-      @order = Order.find(session[:order_id])
-    else
-      @order = Order.new
-    end
-
-    @delivery_points = DeliveryPoint.where.not(latitude: nil, longitude: nil)
-
-    @hash = Gmaps4rails.build_markers(@delivery_points) do |delivery_point, marker|
-      marker.lat delivery_point.latitude
-      marker.lng delivery_point.longitude
-      marker.infowindow render_to_string(partial: "/delivery_points/map_box", :formats => [:html], locals: { delivery_point: delivery_point })
-      # marker.infowindow render_to_string(partial: "/delivery_points/map_box", locals: { flat: flat })
-    end
-    end
-  end
-
-  private
 
   def basket_params
     params.require(:basket).permit(:size, :type)
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:create, :show]
+  before_action :set_delivery_points, only: :pick_address
 
   # @order = Order.find(session[:order_id])
 
@@ -43,10 +44,24 @@ class OrdersController < ApplicationController
     end
   end
 
+  def pick_address
+    if session[:order_id].present?
+    @order = Order.find(session[:order_id])
+    end
+
+    respond_to do |format|
+      format.js {
+                  render :template => "baskets/pick_address.js.erb",
+                  :layout => false
+          }
+    end
+  end
+
   def set_address
     @order = Order.find(session[:order_id])
     @order.delivery_point = DeliveryPoint.find(params[:order][:delivery_point])
     @order.save
+
     respond_to do |format|
       format.js {
              render :template => "orders/confirmation.js.erb",
@@ -56,6 +71,23 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def set_delivery_points
+
+    if session[:order_id].present?
+      @order = Order.find(session[:order_id])
+    end
+
+    @delivery_points = DeliveryPoint.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@delivery_points) do |delivery_point, marker|
+      marker.lat delivery_point.latitude
+      marker.lng delivery_point.longitude
+      marker.infowindow render_to_string(partial: "/delivery_points/map_box", :formats => [:html], locals: { delivery_point: delivery_point })
+      # marker.infowindow render_to_string(partial: "/delivery_points/map_box", locals: { flat: flat })
+    end
+  end
+
   def basket_params
     params.require(:basket).permit(:category, :size, :size_url)
   end
